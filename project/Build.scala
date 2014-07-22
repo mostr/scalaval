e5443917b67aa97cc9fb3736aaf5734849671287
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-07-22 13:36:29 +0200

    Add build-related stuff for release purposes

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,33 @@ object BuildSettings {
   val buildSettings = Defaults.coreDefaultSettings ++ Seq(
     organization := "com.softwaremill.scalaval",
     version := "0.1-SNAPSHOT",
-    scalaVersion := ScalaVersion
+    scalaVersion := ScalaVersion,
+    // Sonatype OSS deployment
+    publishTo := {
+      val nexus = "https://oss.sonatype.org/"
+      if (version.value.trim.endsWith("SNAPSHOT"))
+        Some("snapshots" at nexus + "content/repositories/snapshots")
+      else
+        Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+    },
+    credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
+    publishMavenStyle := true,
+    publishArtifact in Test := false,
+    pomIncludeRepository := { _ => false },
+    pomExtra :=
+      <scm>
+        <url>git@github.com:mostr/scalaval.git</url>
+        <connection>scm:git:git@github.com:mostr/scalaval.git</connection>
+      </scm>
+        <developers>
+          <developer>
+            <id>mostr</id>
+            <name>Michał Ostruszka</name>
+            <url>http://michalostruszka.pl</url>
+          </developer>
+        </developers>,
+    licenses := ("Apache2", new java.net.URL("http://www.apache.org/licenses/LICENSE-2.0.txt")) :: Nil,
+    homepage := Some(new java.net.URL("http://michalostruszka.pl"))
   )
 }
 
